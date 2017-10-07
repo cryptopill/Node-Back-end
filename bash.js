d12bdac194f4fe103d5944f22e2b97860be7bba3
aungkaungmyat <amyat@purdue.edu>
2017-10-07 17:56:56 -0500

    managed to run the script from nodejs

--- bash.js
+++ bash.js
@@ -2,6 +2,20 @@
 var exec = require('child_process').exec;
 
 // function puts(error, stdout, stderr) { util.puts(stdout) }
-exec("node index", function(err, stdout, stderr) {
+// exec("cd /home/olaf/Remote/Madhacks", function(err,stdout,stderr){
+//   console.log('success');
+// })
+console.log('Starting directory ' + process.cwd());
+try {
+  process.chdir('/home/olaf/Remote/Madhacks/');
+  console.log('New directory: ' + process.cwd() + '/./go.sh');
+}
+catch (err) {
+  console.log('chdir: ' + err);
+}
+
+//to run the bash script from node js
+exec(process.cwd() + '/./go.sh', function(err, stdout, stderr) {
+  console.log('hi')
   console.log(stdout);
 });
